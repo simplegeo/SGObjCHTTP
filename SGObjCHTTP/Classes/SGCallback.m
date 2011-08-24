07f12fb10db1e9c2be9ba606cb3735b345285872
dsmith <derek@dsmitts.com>
2011-08-24 12:35:10 -0700

    Silly copy/pasta mistake.

--- SGObjCHTTP/Classes/SGCallback.m
+++ SGObjCHTTP/Classes/SGCallback.m
@@ -9,219 +9,107 @@
 #import "SGCallback.h"
 
 #import "JSONKit.h"
+#import "ASIHTTPRequest.h"
 #import "ASIHTTPRequest+OAuth.h"
 #import "ASIFormDataRequest+OAuth.h"
 
-@interface SGHTTPClient (Private)
+@interface SGCallback (Private)
 
-- (void)handleRequest:(ASIHTTPRequest *)request failure:(BOOL)failed;
-
-- (NSString *)normalizeRequestParams:(NSDictionary *)params;
-- (NSObject *)jsonObjectForResponseData:(NSData *)data;
+#if NS_BLOCKS_AVAILABLE
++ (void)releaseBlocks:(NSArray *)blocks;
+#endif
 
 @end
 
-@implementation SGHTTPClient
-@synthesize consumerKey, consumerSecret, accessToken, accessTokenSecret, verifier;
+@implementation SGCallback
+@synthesize delegate, successMethod, failureMethod;
 
-- (id)initWithConsumerKey:(NSString *)key consumerSecret:(NSString *)secret
-{
-    return [self initWithConsumerKey:key consumerSecret:secret accessToken:nil accessTokenSecret:nil];
-}
+#if NS_BLOCKS_AVAILABLE
+@synthesize successBlock, failureBlock;
+#endif
 
-- (id)initWithAccessToken:(NSString *)token
-{
-    return [self initWithConsumerKey:nil
-                      consumerSecret:nil
-                         accessToken:token
-                   accessTokenSecret:nil];
-}
-
-- (id)initWithConsumerKey:(NSString *)key
-           consumerSecret:(NSString *)secret 
-              accessToken:(NSString *)tokenKey 
-        accessTokenSecret:(NSString *)tokenSecret
+- (id)initWithDelegate:(id)d successMethod:(SEL)sMethod failureMethod:(SEL)fMethod
 {
     self = [super init];
     if(self) {
-        
-        if(key)
-            consumerKey = [key retain];
-        if(secret)
-            consumerSecret = [secret retain];
-        if(tokenKey)
-            accessToken = [tokenKey retain];
-        if(tokenSecret)
-            accessTokenSecret = [tokenSecret retain];
+        delegate = d;
+        successMethod = sMethod;
+        failureMethod = fMethod;
     }
     
-    return self;    
+    return self;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////
-#pragma mark -
-#pragma mark ASIHTTPRequest delegate methods 
-//////////////////////////////////////////////////////////////////////////////////////////////// 
-
-- (void)requestFinished:(ASIHTTPRequest *)request
-{    
-    if(200 <= [request responseStatusCode] && [request responseStatusCode] < 300)
-        [self handleRequest:request failure:NO];
-    else
-        [self handleRequest:request failure:YES];
-}
-
-- (void)requestFailed:(ASIHTTPRequest *)request
++(SGCallback *) callbackWithDelegate:(id)delegate successMethod:(SEL)successMethod failureMethod:(SEL)failureMethod
 {
-    [self handleRequest:request failure:YES];
+    return [[[SGCallback alloc] initWithDelegate:delegate successMethod:successMethod failureMethod:failureMethod] autorelease];
 }
 
-- (void)handleRequest:(ASIHTTPRequest *)request failure:(BOOL)failed
-{
-    // Every request should contain a callback. If it doesn't,
-    // then we are not setup to handle the request.
-    NSDictionary* userInfo = [request userInfo];
-    if(userInfo) {
-        SGCallback *callback = [userInfo objectForKey:@"callback"];
-        if(callback) {
-            NSObject *response = [self jsonObjectForResponseData:[request responseData]];
-            if(failed) {
-                if(!response)
-                    response = [[[NSString alloc] initWithData:[request responseData] encoding:NSUTF8StringEncoding] autorelease];
-                
-                // Add more values to this dictionary in order to provide
-                // more error information to the delegate.
-                NSDictionary *errorInfo = [NSDictionary dictionaryWithObjectsAndKeys:
-                                           response, @"response",
-                                           nil];
-                NSError* error = [NSError errorWithDomain:[request.url absoluteString]
-                                                     code:[request responseStatusCode]
-                                                 userInfo:errorInfo];
-                
-                if(callback && callback.delegate && [callback.delegate respondsToSelector:callback.failureMethod])
-                    [callback.delegate performSelector:callback.failureMethod withObject:error];
-                
 #if NS_BLOCKS_AVAILABLE
-                if(callback.failureBlock)
-                    callback.failureBlock(error);
++ (SGCallback *)callbackWithSuccessBlock:(SGSuccessBlock)sBlock failureBlock:(SGFailureBlock)fBlock
+{
+    return [[[SGCallback alloc] initWithSuccessBlock:sBlock failureBlock:fBlock] autorelease];
+}
+
+- (id)initWithSuccessBlock:(SGSuccessBlock)sBlock failureBlock:(SGFailureBlock)fBlock
+{
+    self = [super init];
+    if(self) {
+        delegate = nil;
+        successBlock = [sBlock copy];
+        failureBlock = [fBlock copy];
+    }
+    
+    return self;
+}
 #endif
-            } else {
-                NSObject *response = [self jsonObjectForResponseData:[request responseData]];
-                if(callback && callback.delegate && [callback.delegate respondsToSelector:callback.successMethod])
-                    [callback.delegate performSelector:callback.successMethod 
-                                            withObject:response];                
-                
+
+- (void)doSuccess:(id)response
+{
+    if(delegate)
+        [delegate performSelector:successMethod withObject:response];
+    
 #if NS_BLOCKS_AVAILABLE
-                if(callback.successBlock)
-                    callback.successBlock(response);
+    if(successBlock)
+        successBlock(response);
 #endif
-            }   
-        }
-    }
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////
-#pragma mark -
-#pragma mark Utility methods
-////////////////////////////////////////////////////////////////////////////////////////////////
-
-- (void)sendHTTPRequest:(NSString *)type
-                  toURL:(NSURL *)url
-             withParams:(id)params 
-               callback:(SGCallback *)callback
-{	
-    // Used in 3-legged oauth2.0
-    if(accessToken && !consumerKey && !consumerSecret) {
-        url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@",
-                                    [url absoluteString],
-                                    [url query] ? @"&" : @"?",
-                                    [self normalizeRequestParams:[NSDictionary dictionaryWithObject:accessToken forKey:@"oauth_token"]]]];
-    }
-    
-    ASIHTTPRequest* request = nil;
-    if([type isEqualToString:@"POST"]) {
-        ASIFormDataRequest *postRequest = [ASIFormDataRequest requestWithURL:url];
-        if([params isKindOfClass:[NSDictionary class]]) {
-            for(NSString *key in [[params allKeys] sortedArrayUsingSelector:@selector(compare:)]) {
-                [postRequest setPostValue:[params objectForKey:key] forKey:key];
-            }
-        } else if([params isKindOfClass:[NSData class]])
-            [postRequest setPostBody:[NSMutableData dataWithData:params]];
-        
-        request = postRequest;
-    } else {
-        NSString *queryParameters = nil;
-        if(params) {
-            if([params isKindOfClass:[NSDictionary class]] && [params count]) {
-                queryParameters = [NSString stringWithFormat:@"%@%@", 
-                                   url.query ? @"&" : @"?",
-                                   [self normalizeRequestParams:params]];
-                
-                url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", [url absoluteString], queryParameters]];               
-            }
-        }
-        
-        request = [ASIHTTPRequest requestWithURL:url];
-        
-        if(params && [params isKindOfClass:[NSData class]])
-            [request setPostBody:[NSMutableData dataWithData:params]];
-    }
-    
-    request.requestMethod = type;
-    if(consumerKey && consumerSecret) {
-        [request signRequestWithClientIdentifier:consumerKey
-                                          secret:consumerSecret
-                                 tokenIdentifier:accessToken
-                                          secret:accessTokenSecret
-                                        verifier:verifier
-                                     usingMethod:ASIOAuthHMAC_SHA1SignatureMethod];
-    }
-    
-    request.userInfo = [NSDictionary dictionaryWithObject:callback forKey:@"callback"];    
-    [request setDelegate:self];
-    [request addRequestHeader:@"Accept" value:@"application/json"];
-    [request startAsynchronous];
-}
-
-- (NSObject *)jsonObjectForResponseData:(NSData *)data
+- (void)doFailure:(NSError *)error
 {
-    NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    NSDictionary *jsonObject = nil;
-    if(jsonString)
-        jsonObject = [jsonString objectFromJSONString];
-    [jsonString release];
-    return jsonObject;
-}
-
-- (NSString *)normalizeRequestParams:(NSDictionary *)params
-{
-    NSMutableArray *parameterPairs = [NSMutableArray arrayWithCapacity:([params count])];
-    id value= nil;
-    for(NSString* param in params) {
-        value = [params objectForKey:param];
-        if([value isKindOfClass:[NSArray class]]) {
-            for(NSString *element in value) {
-                [parameterPairs addObject:[[NSString stringWithFormat:@"%@=%@", param, element]
-                                           stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding]];                
-            }
-        } else {
-            param = [NSString stringWithFormat:@"%@=%@", param, value];
-            [parameterPairs addObject:[param stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding]];
-        }
-    }
+    if(delegate)
+        [delegate performSelector:failureMethod withObject:error];
     
-    NSArray* sortedPairs = [parameterPairs sortedArrayUsingSelector:@selector(compare:)];
-    return [sortedPairs componentsJoinedByString:@"&"];
+#if NS_BLOCKS_AVAILABLE
+    if(failureBlock)
+        failureBlock(error);
+#endif    
 }
 
-- (void) dealloc
+- (void)dealloc
 {
-    [consumerKey release];
-    [accessToken release];
-    [accessTokenSecret release];
-    [consumerSecret release];
-    [verifier release];
+#if NS_BLOCKS_AVAILABLE
+	NSMutableArray *blocks = [NSMutableArray array];
+	if(successBlock) {
+		[blocks addObject:successBlock];
+		[successBlock release];
+		successBlock = nil;
+	}
+	if(failureBlock) {
+		[blocks addObject:failureBlock];
+		[failureBlock release];
+		failureBlock = nil;
+	}    
+    [[self class] performSelectorOnMainThread:@selector(releaseBlocks:) 
+                                   withObject:blocks
+                                waitUntilDone:[NSThread isMainThread]];
     [super dealloc];
 }
+
++ (void)releaseBlocks:(NSArray *)blocks
+{
+	// Blocks will be released when this method exits
+}
+#endif
 
 @end
--- SGObjCHTTP/Classes/SGHTTPClient.m
+++ SGObjCHTTP/Classes/SGHTTPClient.m
@@ -15,103 +15,216 @@
 
 #import "JSONKit.h"
 
-@interface SGCallback (Private)
+@interface SGHTTPClient (Private)
 
-#if NS_BLOCKS_AVAILABLE
-+ (void)releaseBlocks:(NSArray *)blocks;
-#endif
+- (void)handleRequest:(ASIHTTPRequest *)request failure:(BOOL)failed;
+
+- (NSString *)normalizeRequestParams:(NSDictionary *)params;
+- (NSObject *)jsonObjectForResponseData:(NSData *)data;
 
 @end
 
-@implementation SGCallback
-@synthesize delegate, successMethod, failureMethod;
+@implementation SGHTTPClient
+@synthesize consumerKey, consumerSecret, accessToken, accessTokenSecret, verifier;
 
-#if NS_BLOCKS_AVAILABLE
-@synthesize successBlock, failureBlock;
-#endif
+- (id)initWithConsumerKey:(NSString *)key consumerSecret:(NSString *)secret
+{
+    return [self initWithConsumerKey:key consumerSecret:secret accessToken:nil accessTokenSecret:nil];
+}
 
-- (id)initWithDelegate:(id)d successMethod:(SEL)sMethod failureMethod:(SEL)fMethod
+- (id)initWithAccessToken:(NSString *)token
+{
+    return [self initWithConsumerKey:nil
+                      consumerSecret:nil
+                         accessToken:token
+                   accessTokenSecret:nil];
+}
+
+- (id)initWithConsumerKey:(NSString *)key
+           consumerSecret:(NSString *)secret 
+              accessToken:(NSString *)tokenKey 
+        accessTokenSecret:(NSString *)tokenSecret
 {
     self = [super init];
     if(self) {
-        delegate = d;
-        successMethod = sMethod;
-        failureMethod = fMethod;
+        
+        if(key)
+            consumerKey = [key retain];
+        if(secret)
+            consumerSecret = [secret retain];
+        if(tokenKey)
+            accessToken = [tokenKey retain];
+        if(tokenSecret)
+            accessTokenSecret = [tokenSecret retain];
     }
     
-    return self;
+    return self;    
 }
 
-+(SGCallback *) callbackWithDelegate:(id)delegate successMethod:(SEL)successMethod failureMethod:(SEL)failureMethod
+////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark ASIHTTPRequest delegate methods 
+//////////////////////////////////////////////////////////////////////////////////////////////// 
+
+- (void)requestFinished:(ASIHTTPRequest *)request
+{    
+    if(200 <= [request responseStatusCode] && [request responseStatusCode] < 300)
+        [self handleRequest:request failure:NO];
+    else
+        [self handleRequest:request failure:YES];
+}
+
+- (void)requestFailed:(ASIHTTPRequest *)request
 {
-    return [[[SGCallback alloc] initWithDelegate:delegate successMethod:successMethod failureMethod:failureMethod] autorelease];
+    [self handleRequest:request failure:YES];
 }
 
+- (void)handleRequest:(ASIHTTPRequest *)request failure:(BOOL)failed
+{
+    // Every request should contain a callback. If it doesn't,
+    // then we are not setup to handle the request.
+    NSDictionary* userInfo = [request userInfo];
+    if(userInfo) {
+        SGCallback *callback = [userInfo objectForKey:@"callback"];
+        if(callback) {
+            NSObject *response = [self jsonObjectForResponseData:[request responseData]];
+            if(failed) {
+                if(!response)
+                    response = [[[NSString alloc] initWithData:[request responseData] encoding:NSUTF8StringEncoding] autorelease];
+                
+                // Add more values to this dictionary in order to provide
+                // more error information to the delegate.
+                NSDictionary *errorInfo = [NSDictionary dictionaryWithObjectsAndKeys:
+                                           response, @"response",
+                                           nil];
+                NSError* error = [NSError errorWithDomain:[request.url absoluteString]
+                                                     code:[request responseStatusCode]
+                                                 userInfo:errorInfo];
+                
+                if(callback && callback.delegate && [callback.delegate respondsToSelector:callback.failureMethod])
+                    [callback.delegate performSelector:callback.failureMethod withObject:error];
+                
 #if NS_BLOCKS_AVAILABLE
-+ (SGCallback *)callbackWithSuccessBlock:(SGSuccessBlock)sBlock failureBlock:(SGFailureBlock)fBlock
-{
-    return [[[SGCallback alloc] initWithSuccessBlock:sBlock failureBlock:fBlock] autorelease];
+                if(callback.failureBlock)
+                    callback.failureBlock(error);
+#endif
+            } else {
+                NSObject *response = [self jsonObjectForResponseData:[request responseData]];
+                if(callback && callback.delegate && [callback.delegate respondsToSelector:callback.successMethod])
+                    [callback.delegate performSelector:callback.successMethod 
+                                            withObject:response];                
+                
+#if NS_BLOCKS_AVAILABLE
+                if(callback.successBlock)
+                    callback.successBlock(response);
+#endif
+            }   
+        }
+    }
 }
 
-- (id)initWithSuccessBlock:(SGSuccessBlock)sBlock failureBlock:(SGFailureBlock)fBlock
-{
-    self = [super init];
-    if(self) {
-        delegate = nil;
-        successBlock = [sBlock copy];
-        failureBlock = [fBlock copy];
+////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Utility methods
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+- (void)sendHTTPRequest:(NSString *)type
+                  toURL:(NSURL *)url
+             withParams:(id)params 
+               callback:(SGCallback *)callback
+{	
+    // Used in 3-legged oauth2.0
+    if(accessToken && !consumerKey && !consumerSecret) {
+        url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@%@",
+                                    [url absoluteString],
+                                    [url query] ? @"&" : @"?",
+                                    [self normalizeRequestParams:[NSDictionary dictionaryWithObject:accessToken forKey:@"oauth_token"]]]];
     }
     
-    return self;
-}
-#endif
-
-- (void)doSuccess:(id)response
-{
-    if(delegate)
-        [delegate performSelector:successMethod withObject:response];
-
-#if NS_BLOCKS_AVAILABLE
-    if(successBlock)
-       successBlock(response);
-#endif
-}
-
-- (void)doFailure:(NSError *)error
-{
-    if(delegate)
-        [delegate performSelector:failureMethod withObject:error];
+    ASIHTTPRequest* request = nil;
+    if([type isEqualToString:@"POST"]) {
+        ASIFormDataRequest *postRequest = [ASIFormDataRequest requestWithURL:url];
+        if([params isKindOfClass:[NSDictionary class]]) {
+            for(NSString *key in [[params allKeys] sortedArrayUsingSelector:@selector(compare:)]) {
+                [postRequest setPostValue:[params objectForKey:key] forKey:key];
+            }
+        } else if([params isKindOfClass:[NSData class]])
+            [postRequest setPostBody:[NSMutableData dataWithData:params]];
+        
+        request = postRequest;
+    } else {
+        NSString *queryParameters = nil;
+        if(params) {
+            if([params isKindOfClass:[NSDictionary class]] && [params count]) {
+                queryParameters = [NSString stringWithFormat:@"%@%@", 
+                                   url.query ? @"&" : @"?",
+                                   [self normalizeRequestParams:params]];
+                
+                url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@", [url absoluteString], queryParameters]];               
+            }
+        }
+        
+        request = [ASIHTTPRequest requestWithURL:url];
+        
+        if(params && [params isKindOfClass:[NSData class]])
+            [request setPostBody:[NSMutableData dataWithData:params]];
+    }
     
-#if NS_BLOCKS_AVAILABLE
-    if(failureBlock)
-        failureBlock(error);
-#endif    
+    request.requestMethod = type;
+    if(consumerKey && consumerSecret) {
+        [request signRequestWithClientIdentifier:consumerKey
+                                          secret:consumerSecret
+                                 tokenIdentifier:accessToken
+                                          secret:accessTokenSecret
+                                        verifier:verifier
+                                     usingMethod:ASIOAuthHMAC_SHA1SignatureMethod];
+    }
+    
+    request.userInfo = [NSDictionary dictionaryWithObject:callback forKey:@"callback"];    
+    [request setDelegate:self];
+    [request addRequestHeader:@"Accept" value:@"application/json"];
+    [request startAsynchronous];
 }
 
-- (void)dealloc
+- (NSObject *)jsonObjectForResponseData:(NSData *)data
 {
-#if NS_BLOCKS_AVAILABLE
-	NSMutableArray *blocks = [NSMutableArray array];
-	if(successBlock) {
-		[blocks addObject:successBlock];
-		[successBlock release];
-		successBlock = nil;
-	}
-	if(failureBlock) {
-		[blocks addObject:failureBlock];
-		[failureBlock release];
-		failureBlock = nil;
-	}    
-    [[self class] performSelectorOnMainThread:@selector(releaseBlocks:) 
-                                   withObject:blocks
-                                waitUntilDone:[NSThread isMainThread]];
+    NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSDictionary *jsonObject = nil;
+    if(jsonString)
+        jsonObject = [jsonString objectFromJSONString];
+    [jsonString release];
+    return jsonObject;
+}
+
+- (NSString *)normalizeRequestParams:(NSDictionary *)params
+{
+    NSMutableArray *parameterPairs = [NSMutableArray arrayWithCapacity:([params count])];
+    id value= nil;
+    for(NSString* param in params) {
+        value = [params objectForKey:param];
+        if([value isKindOfClass:[NSArray class]]) {
+            for(NSString *element in value) {
+                [parameterPairs addObject:[[NSString stringWithFormat:@"%@=%@", param, element]
+                                           stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding]];                
+            }
+        } else {
+            param = [NSString stringWithFormat:@"%@=%@", param, value];
+            [parameterPairs addObject:[param stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding]];
+        }
+    }
+    
+    NSArray* sortedPairs = [parameterPairs sortedArrayUsingSelector:@selector(compare:)];
+    return [sortedPairs componentsJoinedByString:@"&"];
+}
+
+- (void) dealloc
+{
+    [consumerKey release];
+    [accessToken release];
+    [accessTokenSecret release];
+    [consumerSecret release];
+    [verifier release];
     [super dealloc];
 }
-
-+ (void)releaseBlocks:(NSArray *)blocks
-{
-	// Blocks will be released when this method exits
-}
-#endif
 
 @end
